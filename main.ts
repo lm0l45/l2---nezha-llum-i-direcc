a8c2dac2548827f5b4a5a2ed707e1226057479f8
lm0l45 <107305695+lm0l45@users.noreply.github.com>
2023-09-05 18:10:45 +0200

    Actualització pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,23 @@ function Esquerra () {
     neZha.setMotorSpeed(neZha.MotorList.M1, -60)
     neZha.setMotorSpeed(neZha.MotorList.M4, -100)
 }
+input.onButtonPressed(Button.A, function () {
+    Esquerra()
+})
+input.onButtonPressed(Button.AB, function () {
+    PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J4, true)
+    PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, false)
+    Endavant()
+})
+input.onButtonPressed(Button.B, function () {
+    Dreta()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.SmallDiamond)
+    neZha.stopAllMotor()
+    PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J4, false)
+    PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, true)
+})
 function Dreta () {
     basic.showArrow(ArrowNames.East)
     neZha.setMotorSpeed(neZha.MotorList.M1, -100)
@@ -16,24 +33,5 @@ function Dreta () {
 basic.showIcon(IconNames.Diamond)
 basic.pause(1000)
 basic.forever(function () {
-    serial.writeValue("direcció", PlanetX_Basic.trimpot(PlanetX_Basic.AnalogRJPin.J2))
-    serial.writeValue("llum", input.lightLevel())
-})
-basic.forever(function () {
-    if (input.lightLevel() > 200) {
-        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J4, true)
-        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, false)
-        if (PlanetX_Basic.trimpot(PlanetX_Basic.AnalogRJPin.J2) < 341) {
-            Esquerra()
-        } else if (PlanetX_Basic.trimpot(PlanetX_Basic.AnalogRJPin.J2) > 682) {
-            Dreta()
-        } else {
-            Endavant()
-        }
-    } else {
-        basic.showIcon(IconNames.SmallDiamond)
-        neZha.stopAllMotor()
-        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J4, false)
-        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, true)
-    }
+	
 })
